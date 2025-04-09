b742183153006941f63a88c07133515dbe291182
Ferchox45 <fer.con0956@gmail.com>
2025-04-08 20:16:26 -0600

    Latest changes made to the login.

--- app/src/main/java/com/example/tiendita/LoginResponse.kt
+++ app/src/main/java/com/example/tiendita/LoginResponse.kt
@@ -3,5 +3,4 @@ package com.example.tiendita
 data class LoginResponse(
     val token: String,
     val user: Usuario
-
 )
--- app/src/main/java/com/example/tiendita/Usuario.kt
+++ app/src/main/java/com/example/tiendita/Usuario.kt
@@ -1,9 +1,11 @@
 package com.example.tiendita
 
+import com.google.gson.annotations.SerializedName
+
 data class Usuario(
-    val id: Int,
+    @SerializedName("_id") val id: String,
     val nombre: String,
-    val apellido: String,
     val email: String,
-    val password: String
 )
+
+
